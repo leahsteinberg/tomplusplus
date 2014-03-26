3eaa02bf4a05bc12260fa45f9a1d7f26c0fa3fbc
Bhushan Lodha <bhushanlodha@gmail.com>
2014-03-26 18:47:31 -0400

    it works, thanks to bhushan

--- repl.c
+++ repl.c
@@ -4,21 +4,40 @@
 #include <ctype.h>
 #define MAXLINE 2000
 
+#define BUFSIZE 10
 typedef struct tnode *Treeptr;
 typedef struct tnode {
   char *type;
-  char value;
+  char *value;
   Treeptr left;
   Treeptr right;
 } Treenode;
 
+Treenode* new_treenode();
+
+Treenode* new_treenode() {
+	Treenode* this = malloc(sizeof(Treenode));
+	this->type = malloc(BUFSIZE);
+	this->value = malloc(BUFSIZE);
+	this->left = NULL;
+	this->right = NULL;
+	return this;
+}
+
+void del_treenode(Treenode* this) {
+	free(this->type);
+	free(this->value);
+	// if (this->left != NULL) {
+	// del_treenode(this->left);
+	free(this);
+}
 void parse(char *buffer);
 void get_input();
-Treenode assign(char *buffer, int *ip);
-Treenode expression(char *buffer, int *ip);
-Treenode phrase(char *buffer, int *ip);
-Treenode leaf(char *buffer, int *ip);
-
+Treenode assign(char **buffer, int *ip);
+Treenode expression(char **buffer, int *ip);
+Treenode phrase(char **buffer, int *ip);
+Treenode leaf(char **buffer, int ip);
+char** tokenize(char *);
 
 int main(){
 
@@ -33,7 +52,7 @@ void get_input(){
    fgets(buffer, MAXLINE, stdin);
    if (!(strncmp(buffer, "quit()", 6)))
      break;
-   parse(buffer);
+  parse(buffer);
   }
 
 
@@ -50,20 +69,58 @@ for (i=0, j=0; i < strlen(buffer); i++) {
 }
 trimmed_buffer[j] = '\0';
 
+char **tokens = tokenize(trimmed_buffer);
 i=0;
 int *ip;
 ip = &i;
 
 Treenode the_tree;
-the_tree = assign(trimmed_buffer, ip);
+the_tree = assign(tokens, ip);
 
 
 }
 
-Treenode assign(char *buffer, int *ip){
+char **tokenize(char* buffer){
+	char *tokens[20];
+	int i, last_op, token_i, j;
+	token_i = 0;
+	for (i=0, last_op = 0; i < strlen(buffer); i++){
+		if(buffer[i] == '='){
+			char new_string[10];
+			for (j=last_op; j<i; j++){
+				new_string[j-last_op] = buffer[j];
+				//	tokens[token_i][j-last_op] = buffer[j];
+			}
+			new_string[j-last_op] = '\0';
+			tokens[token_i] = new_string;
+			token_i++;
+			char op_string[5];
+			op_string[0] = buffer[i];
+			op_string[1] = '\0';
+			tokens[token_i] = op_string;
+			last_op = i;
+			token_i++;
+		}
+	}
+	char new_string[10];
+	for (i = last_op+1; i < strlen(buffer); i++) {
+		new_string[i-last_op-1] = buffer[i];
+	}
+	new_string[i] = '\0';
+	tokens[token_i] = new_string;
+	/* int p, r; */
+	/* for(p=0; p<len(tokens); p++){ */
+	/* 	for(r=0; r */
+	/* } */
+	return tokens;
+}
+
+
+
+Treenode assign(char **buffer, int *ip){
 // Assign -> variable = Expression
-  Treenode var_leaf;
-  var_leaf = leaf(buffer, ip);
+  new_treenode var_leaf;
+  var_leaf = leaf(buffer, *ip);
   (*ip)++;
   //if (buffer[i] == '='){
     //Treenode equalNode;
@@ -76,7 +133,7 @@ Treenode assign(char *buffer, int *ip){
  // }
  // else{
  // }
-  printf("%c\n", var_leaf.value);
+  //printf("%s\n", var_leaf.value);
   return var_leaf;
    }
 
@@ -88,16 +145,20 @@ Treenode assign(char *buffer, int *ip){
 /*   // Phrase -> leaf | leaf * phrase | leaf / phrase */
 /* } */
 
-Treenode leaf(char *buffer, int *ip){
+Treenode leaf(char **buffer, int ip){
   // leaf -> variable | number
   Treenode leafnode;
-  if(isalpha(buffer[*ip])){
-    leafnode.type = "var";
-      }
-  else if(isdigit(buffer[*ip])){
-      leafnode.type = "number";
-    }
-    leafnode.value = buffer[*ip];
+   leafnode = new_treenode();
+   leafnode.value = buffer[ip];
+   char first_value_char = leafnode.value[0];
+   switch (first_value_char){
+		   case 'a' ... 'z':
+    			leafnode.type = "var";
+		   	break;
+		   case '0' ... '9':
+			leafnode.type = "number";
+		   	break;
+		   }
     leafnode.left = NULL;
     leafnode.right = NULL;
     return leafnode;
