65c85bcec9e33e8830319fb6444f40e7517536ac
Bhushan Lodha <bhushanlodha@gmail.com>
2014-03-29 17:27:01 -0400

    tree is reverse, how dumb are we to realize that now

--- repl.c
+++ repl.c
@@ -8,10 +8,10 @@
 int buffer_index = 0;
 typedef struct tnode *Treeptr;
 typedef struct tnode {
-  char *type;
-  char *value;
-  Treeptr left;
-  Treeptr right;
+	char *type;
+	char *value;
+	Treeptr left;
+	Treeptr right;
 } Treenode;
 
 Treenode* new_treenode();
@@ -70,41 +70,61 @@ void parse(char *buffer) {
 	char **tokens = tokenize(trimmed_buffer);
 	Treenode* the_tree;
 	the_tree = assign(tokens);
-	for (int i = 0; i < 200; i++) {
-		free(tokens[i]);
-	}
-	free(tokens);
+	/* for (int i = 0; i < 200; i++) { */
+	/* 	free(tokens[i]); */
+	/* } */
+	/* free(tokens); */
+	/* buffer_index = 0; */
 }
 
 char **tokenize(char* buffer){
 	char** tokens;
 
 	tokens = (char**)malloc(200 * sizeof(char*));
-	for (int i = 0; i < 200; i++) {
-		tokens[i] = (char*)malloc(5);
+	for (int k = 0; k < 200; k++) {
+		tokens[k] = (char*)malloc(5);
 	}
 
 
 	int i, last_op, token_i, j;
 	token_i = 0;
-	for (i=0, last_op = 0; i < strlen(buffer); i++){
-		if(buffer[i] == '=' || buffer[i] == '*'|| buffer[i]=='/' ||buffer[i]=='+' || buffer[i]=='-' || buffer[i] == '(' || buffer[i] == ')'){
-			for (j=last_op; j<i; j++){
-				tokens[token_i][j-last_op] = buffer[j];
+	/* for (i=0, last_op = 0; i < strlen(buffer); i++){ */
+	/* 	if(buffer[i] == '=' || buffer[i] == '*'|| buffer[i]=='/' ||buffer[i]=='+' || buffer[i]=='-' || buffer[i] == '(' || buffer[i] == ')'){ */
+	/* 		if (i == 0 || (isalpha(buffer[i]) || isdigit(buffer[i]))) { */
+	/* 		for (j=last_op; j<i; j++){ */
+	/* 			tokens[token_i][j-last_op] = buffer[j]; */
+	/* 		} */
+	/* 		tokens[token_i][j-last_op] = '\0'; */
+	/* 		token_i++; */
+	/* 		} */
+	/* 		tokens[token_i][0] = buffer[i]; */
+	/* 		tokens[token_i][1] = '\0'; */
+	/* 		last_op = i + 1; */
+	/* 		token_i++; */
+	/* 	} */
+	/* } */
+
+	for (i=0, j = 0; i < strlen(buffer); i++) {
+		if(isalpha(buffer[i]) || isdigit(buffer[i])){
+			while (isalpha(buffer[i]) || isdigit(buffer[i])) {
+				tokens[token_i][j++] = buffer[i++];
 			}
-			tokens[token_i][j-last_op] = '\0';
-			token_i++;
-			tokens[token_i][0] = buffer[i];
-			tokens[token_i][1] = '\0';
-			last_op = i + 1;
-			token_i++;
+			--i;
 		}
+		else{
+			tokens[token_i][j++] = buffer[i];
+		}
+		tokens[token_i][j] = '\0';
+		j=0;
+		token_i++;
 	}
+
+
 	//char new_string[10];
-	for (i = last_op; i < strlen(buffer); i++) {
-		tokens[token_i][i-last_op] = buffer[i];
-	}
-	tokens[token_i][i] = '\0';
+	/* for (i = last_op; i < strlen(buffer); i++) { */
+	/* 	tokens[token_i][i-last_op] = buffer[i]; */
+	/* } */
+	/* tokens[token_i][i] = '\0'; */
 	//tokens[token_i] = new_string;
 	/* int p, r; */
 	/* for(p=0; p<len(tokens); p++){ */
@@ -165,21 +185,22 @@ Treenode *expression(char **buffer){
 /* } */
 
 Treenode *phrase(char **buffer) {
-	Treenode *leafnode = leaf(buffer);
-	buffer_index++;
+	/* Treenode *leafnode = leaf(buffer); */
+	/* buffer_index++; */
 	Treenode *multdivNode;
 	if (!(strncmp(buffer[buffer_index], "*", 1)) || !strncmp(buffer[buffer_index], "/", 1)) {
+		multdivNode->value = buffer[buffer_index];
 		multdivNode = new_treenode();
 		multdivNode->type = "op";
-		multdivNode->value = buffer[buffer_index];
-		multdivNode->left = leafnode;
+		buffer_index--;
+		Treenode *phraseNode = phrase(buffer);
+		multiNode->left = phreaseNode;
 	}
 	else{
 		buffer_index--;
 		return leafnode;
 	}
 	buffer_index++;
-	Treenode *phraseNode = phrase(buffer);
 	multdivNode->right = phraseNode;
 
 
@@ -188,7 +209,20 @@ Treenode *phrase(char **buffer) {
 }
 
 Treenode *leaf(char **buffer){
+
 	// leaf -> variable | number
+	if(buffer[buffer_index][0] == '('){
+		buffer_index++;
+		Treenode *exprNode = expression(buffer);
+		buffer_index++; 
+		if(buffer[buffer_index][0] == ')'){
+			//buffer_index++; // i think??
+			return exprNode;
+		}
+		else{
+			printf("%s", "didnt close parentheses!!!!\n");
+		}
+	}
 	Treenode* leafnode = new_treenode();
 	leafnode->value = buffer[buffer_index];
 	char first_value_char = leafnode->value[0];
@@ -204,4 +238,3 @@ Treenode *leaf(char **buffer){
 	leafnode->right = NULL;
 	return leafnode;
 }
-
